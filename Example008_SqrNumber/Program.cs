072b3a350fae4c2c2a7239e54582b91d5a933567
Trofpa <yaseinokaminari@gmail.com>
2022-10-01 14:33:01 +0300

    markdown files ignored; Example008_SqrNumber is added

---
+++ Example008_SqrNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+Задача нахождения квадрата введенного числа
+*/
+
+Console.Write("Введите, пожалуйста, вещественное число: ");
+
+double number = Convert.ToDouble(Console.ReadLine());
+
+double square = Math.Pow(number, 2);
+
+Console.WriteLine("Квадрат числа: " + square);
