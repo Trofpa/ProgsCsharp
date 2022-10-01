b15cb725842a1d53b513588bb080cbb427b41c71
Trofpa <yaseinokaminari@gmail.com>
2022-10-01 14:53:57 +0300

    Task1 renamed

--- Csharp_seminar1/Task1_Square/Program.cs
+++ Csharp_seminar1/Task1_Square/Program.cs
@@ -5,7 +5,6 @@
 Console.Write("Введите, пожалуйста, вещественное число: ");
 
 double number = Convert.ToDouble(Console.ReadLine());
-
 double square = Math.Pow(number, 2);
 
 Console.WriteLine("Квадрат числа: " + square);
