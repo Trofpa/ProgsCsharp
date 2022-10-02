e99fba9d2e88829fe831c7640cd5025c9d800304
Trofpa <yaseinokaminari@gmail.com>
2022-10-02 11:21:22 +0300

    Task2 in Homework updated Added check for the introduction of numbers (not chars) and their equality

--- Csharp_seminar1/Homework/Task2_BiggerLesserNumber/Program.cs
+++ Csharp_seminar1/Homework/Task2_BiggerLesserNumber/Program.cs
@@ -1,11 +1,38 @@
-﻿int a = 1;
-int b = 2;
+﻿/*
+Условие задачи:
+Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-if(a > b)
+Автор: Трофимов П.А.
+https://github.com/Trofpa/ProgsCsharp/tree/sem1_homework/Csharp_seminar1/Homework/Task2_BiggerLesserNumber
+*/
+Console.Clear();
+
+int FirstNumber;
+int SecondNumber;
+Console.WriteLine("Введите первое число: "); // Вводим числа
+int.TryParse(Console.ReadLine(), out FirstNumber);
+Console.WriteLine("Введите второе число: ");
+int.TryParse(Console.ReadLine(), out SecondNumber);
+
+if ((i != 0)&& (j != 0)) // Проверка на то, что были введены числа
 {
-    Console.WriteLine("Максимальное число: " + a + "\nМинимальное число: " + b);
+    if(FirstNumber > SecondNumber) // Сравниваем введенные числа
+    {
+        Console.WriteLine("Большее число: " + FirstNumber + "\nМеньшее число: " + SecondNumber);
+    }
+    else 
+    {
+        if (FirstNumber == SecondNumber)
+        {
+        Console.WriteLine("\nВведенные числа равны!"); // Проверка на равенство введенных чисел
+        }
+        else
+        {
+        Console.WriteLine("\nБольшее число: " + SecondNumber + "\nМеньшее число: " + FirstNumber);
+        }
+    }
 }
 else
 {
-    Console.WriteLine("Максимальное число: " + b + "\nМинимальное число: " + a);
+    Console.WriteLine("Одно или оба значения не являлись числом!");
 }
